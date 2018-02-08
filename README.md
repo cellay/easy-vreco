44cbc8513d4318ae75258063f5ce2dafeadf3301
cellay <camposellay@gmail.com>
2018-02-08 07:06:49 -0500

    Cambiando estilos, agregando readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Reto 01: Easy Vreco
+## Instrucciones
++ En este reto debemos usar la API Geolocation de HTML5 y Google Maps API
++ El diseño debe ser en grillas y puedes usar cualquier framework
++ Es necesaria la vista desktop y mobile
+## Autores
+Ornella Campos
+Brenda Mesías
--- js/app.js
+++ js/app.js
@@ -1,66 +1,64 @@
 function initMap() {
-    let map;
-    let myPosition = { lat: 0, lng: 0 };
-    let start = document.getElementById('start');
-    let finish = document.getElementById('finish');
+  let map;
+  let myPosition = { lat: 0, lng: 0 };
+  let start = document.getElementById('start');
+  let finish = document.getElementById('finish');
 
-    //Autocompletado
-    new google.maps.places.Autocomplete(start);
-    new google.maps.places.Autocomplete(finish);
+  //  Autocompletado
+  new google.maps.places.Autocomplete(start);
+  new google.maps.places.Autocomplete(finish);
 
-    //Direccciones
-    let directionsService = new google.maps.DirectionsService;
-    let directionsDisplay = new google.maps.DirectionsRenderer;
+  //  Direccciones
+  let directionsService = new google.maps.DirectionsService;
+  let directionsDisplay = new google.maps.DirectionsRenderer;
 
-    document.getElementById('find').addEventListener('click', findMe);
+  document.getElementById('find').addEventListener('click', findMe);
 
-    //Mapa de inicio
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: myPosition,
-        zoom: 2
+  //  Mapa de inicio
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: myPosition,
+    zoom: 2
+  });
+
+  function findMe() {
+    let success = ({ coords }) => {
+      myPosition = { lat: coords.latitude, lng: coords.longitude };
+      let image = 'assets/images/bike.png';
+      let marker = new google.maps.Marker({
+        position: myPosition,
+        map,
+        icon: image
+      });
+      map.setZoom(18);
+      map.setCenter(myPosition);
+    }
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success);
+    }
+  }
+
+  //  Imprimir ruta
+  let trackRoute = () => {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+    map = new google.maps.Map(document.getElementById('map')); 0
+    directionsDisplay.setMap(map);
+    start.value = '';
+    finish.value = '';
+  };
+
+  document.getElementById('track').addEventListener('click', trackRoute);
+
+  let calculateAndDisplayRoute = (directionsService, directionsDisplay) => {
+    directionsService.route({
+      origin: start.value,
+      destination: finish.value,
+      travelMode: 'DRIVING'
+    }, (response, status) => {
+      if (status === 'OK') {
+        directionsDisplay.setDirections(response);
+      } else {
+        window.alert(status);
+      }
     });
-
-    function findMe() {
-        let success = ({coords}) => {
-            myPosition = { lat: coords.latitude, lng: coords.longitude };
-            let image = 'assets/images/bike.png';
-            let marker = new google.maps.Marker({
-                position: myPosition,
-                map,
-                icon: image
-            });
-            map.setZoom(18);
-            map.setCenter(myPosition);
-        }
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(success);
-        }
-    }
-
-    //Imprimir ruta
-
-
-    let trackRoute = () => {
-        calculateAndDisplayRoute(directionsService, directionsDisplay);
-        map = new google.maps.Map(document.getElementById('map')); 0
-        directionsDisplay.setMap(map);
-        start.value = '';
-        finish.value = '';
-    };
-
-    document.getElementById('track').addEventListener('click', trackRoute);
-
-    let calculateAndDisplayRoute = (directionsService, directionsDisplay) => {
-        directionsService.route({
-            origin: start.value,
-            destination: finish.value,
-            travelMode: 'DRIVING'
-        }, (response, status) => {
-            if (status === 'OK') {
-                directionsDisplay.setDirections(response);
-            } else {
-                window.alert(status);
-            }
-        });
-    }
+  }
 }
